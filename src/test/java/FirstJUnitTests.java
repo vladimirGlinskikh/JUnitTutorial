d92bed1d610b3fc6acfca307155573676fb1b8e6
Vladimir Glinskikh <support@zhelezyaka.kz>
2018-12-11 12:26:53 +0600

    Write first example test.

---
+++ src/test/java/FirstJUnitTests.java
@@ -0,0 +1,9 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class FirstJUnitTests {
+    @Test
+    void myFirstTest() {
+        assertEquals(2, 1 + 1);
+    }
+}
