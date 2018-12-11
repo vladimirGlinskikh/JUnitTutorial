1d5e923dfc369fdb951668d0976b96ed1988bb3f
Vladimir Glinskikh <support@zhelezyaka.kz>
2018-12-11 12:38:03 +0600

    write @Disable annotation.

--- src/test/java/FirstJUnitTests.java
+++ src/test/java/FirstJUnitTests.java
@@ -1,9 +1,17 @@
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class FirstJUnitTests {
+    @Disabled
     @Test
     void myFirstTest() {
         assertEquals(2, 1 + 1);
+    }
+
+    @Test
+    void mySecondTest() {
+        assertEquals("Word", "Word");
     }
 }
