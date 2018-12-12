b78fe081fdf3570a9e3a262896e4e29e780dae44
VladimirGlinskikh <support@zhelezyaka.kz>
2018-12-12 15:41:06 +0600

    Display name

---
+++ src/test/java/DisplayNameDemo.java
@@ -0,0 +1,19 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("A special test case")
+class DisplayNameDemo {
+    @Test
+    @DisplayName("Custom test name containing spaces")
+    void testWithDisplayNameContainingSpaces(){
+
+    }
+    @Test
+    @DisplayName("╯°□°）╯")
+    void testWithDisplayNameContainingSpecialCharacters(){
+
+    }
+    @Test
+    @DisplayName("\uD83D\uDE31")
+    void testWithDisplayNameContainingEmoji(){}
+}
